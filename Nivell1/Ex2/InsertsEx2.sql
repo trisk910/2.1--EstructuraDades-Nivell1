fcab4e4c387731eccbefe3180e7148e74d9a9ba0
Marc Bernabeu Rodriguez <marc_b_r@hotmail.com>
2024-11-21 12:31:30 +0100

    correció dels exercicis fet

--- Nivell1/Ex2/InsertsEx2.sql
+++ Nivell1/Ex2/InsertsEx2.sql
@@ -1,11 +1,11 @@
-INSERT INTO `ex2`.`state` (`state_name`) VALUES ('Barcelona'), ('Girona'), ('Tarragona'), 
+INSERT INTO `s11Pizzeria`.`state` (`state_name`) VALUES ('Barcelona'), ('Girona'), ('Tarragona'), 
 ('Lleida');
 
-INSERT INTO `ex2`.`location` (`location_name`, `state_state_id`) VALUES 
+INSERT INTO `s11Pizzeria`.`location` (`location_name`, `state_state_id`) VALUES 
     ('Castelldefels',1), ('Espot',2), ('Reus',3), ('Tàrrega',4);
 
     
-INSERT INTO `ex2`.`client` (`client_name`, `client_surname`, `client_address`, `client_zip`, `client_number`, `location_location_id`) 
+INSERT INTO `s11Pizzeria`.`client` (`client_name`, `client_surname`, `client_address`, `client_zip`, `client_phone`, `location_location_id`) 
 VALUES 
     ('Ernest','Bergada','Carrer Arcadi 30','08860','666555544',1),
     ('Marta', 'Serra', 'Carrer Major 45', '25597', '777888999', 2),
@@ -13,44 +13,35 @@ VALUES
     ('Martí','Babau','Carrer Grantoa 12','25300','333444455',4);
 
     
-INSERT INTO `ex2`.`order` (`order_date`, `order_orderType`, `order_price`, `client_client_id`) 
+INSERT INTO `s11Pizzeria`.`order` (`order_date`, `order_orderType`, `order_price`,`order_quantity`,  `client_client_id`) 
 VALUES 
-    ('2024-10-01','Delivery',25,1),
-    ('2024-10-06','Onsite',15,2),
-    ('2024-10-13','Delivery',65,3),
-    ('2024-10-18','Onsite',32,4),
-    ('2024-10-30','Onsite',8,4);
+    ('2024-10-01','Delivery',25,2,1),
+    ('2024-10-06','Onsite',15,4,2),
+    ('2024-10-13','Delivery',65,8,3),
+    ('2024-10-18','Onsite',32,6,4),
+    ('2024-10-30','Onsite',8,2,4);
 
     
-INSERT INTO `ex2`.`category` (`category_name`) VALUES ('Tradicionals'), ('Especials'),
+INSERT INTO `s11Pizzeria`.`category` (`category_name`) VALUES ('Tradicionals'), ('Especials'),
 ('Hamburgues'), ('Beguda');
 
-INSERT INTO `ex2`.`product` (`product_name`, `product_description`, `product_image`, `product_price`, `category_category_id`) 
-VALUES 
-    ('Pizza 4 Formatges','4 formatges diferents','foto',12,1),
-    ('Pizza 4 Estacions','diferents ingredients','foto',14,1),
-    ('Pizza Estrellada','Ingredients especials amb un ou','foto',15,2),
-    ('Pizza Carnivora','diferents tipus de carn','foto',16,2),
-    ('Hamburgues Trufada','carn de bou amb truga','foto',13,3),
-    ('Refresc','beguda refrescan','foto',3,4);
 
-
-INSERT INTO `ex2`.`product` (`product_name`,`product_description`,`product_image`,`product_price`,
+INSERT INTO `s11Pizzeria`.`product` (`product_name`,`product_description`,`product_image`,`product_price`,`product_type`,
 `category_category_id`)
 VALUES
-	('Pizza 4 Formatges','4 formatges diferents','foto',12,1),
-	('Pizza 4 Estacions','diferents ingredients','foto',14,1),
-	('Pizza Estrellada','Ingredients especials amb un ou','foto',15,2),
-	('Pizza Carnivora','diferents tipus de carn','foto',16,2),
-	('Hamburgues Trufada','carn de bou amb truga','foto',13,3),
-	('Refresc','beguda refrescan','foto',3,4);
+	('Pizza 4 Formatges','4 formatges diferents','foto',12,'pizza',1),
+	('Pizza 4 Estacions','diferents ingredients','foto',14,'pizza',1),
+	('Pizza Estrellada','Ingredients especials amb un ou','foto',15,'pizza',2),
+	('Pizza Carnivora','diferents tipus de carn','foto',16,'pizza',2),
+	('Hamburgues Trufada','carn de bou amb truga','foto',13,'hamburguesa',3),
+	('Refresc','beguda refrescan','foto',3,'beguda',4);
 
-INSERT INTO `ex2`.`order_details` (`order_details_quantity`, `order_order_id`, `product_product_id`) 
+INSERT INTO `s11Pizzeria`.`order_details` (`order_details_quantity`, `order_order_id`, `product_product_id`) 
 VALUES 
     (2,1,1), (3,2,2), (1,3,3), (5,4,5), (2,5,6);
 
 
-INSERT INTO `ex2`.`store` (`store_address`, `store_zip`, `location_location_id`, `order_order_id`) 
+INSERT INTO `s11Pizzeria`.`store` (`store_address`, `store_zip`, `location_location_id`, `order_order_id`) 
 VALUES 
     ('carrer major 40', '08860',1,1),
     ('carrer valencia 10', '25597',2,2),
@@ -58,7 +49,7 @@ VALUES
     ('carrer mallorca 23', '25300',4,4);
 
     
-INSERT INTO `ex2`.`worker` (`worker_name`,`worker_surname`,`worker_nif`,`worker_number`,
+INSERT INTO `s11Pizzeria`.`worker` (`worker_name`,`worker_surname`,`worker_nif`,`worker_phone`,
 `worker_type`,`store_store_id`)
 VALUES
     ('Anna', 'Ribas', 'A12345678', '600123456', 'chef', 1),
--- Nivell1/Ex2/pizzeriaTaules.sql
+++ Nivell1/Ex2/pizzeriaTaules.sql
@@ -5,15 +5,15 @@ SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0;
 SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='ONLY_FULL_GROUP_BY,STRICT_TRANS_TABLES,NO_ZERO_IN_DATE,NO_ZERO_DATE,ERROR_FOR_DIVISION_BY_ZERO,NO_ENGINE_SUBSTITUTION';
 
 -- -----------------------------------------------------
--- Schema Ex2
+-- Schema s11Pizzeria
 -- -----------------------------------------------------
-CREATE SCHEMA IF NOT EXISTS `Ex2` DEFAULT CHARACTER SET utf8 ;
-USE `Ex2`;
+CREATE SCHEMA IF NOT EXISTS `s11Pizzeria` DEFAULT CHARACTER SET utf8 ;
+USE `s11Pizzeria`;
 
 -- -----------------------------------------------------
--- Table `Ex2`.`state`
+-- Table `s11Pizzeria`.`state`
 -- -----------------------------------------------------
-CREATE TABLE IF NOT EXISTS `Ex2`.`state` (
+CREATE TABLE IF NOT EXISTS `s11Pizzeria`.`state` (
   `state_id` INT NOT NULL AUTO_INCREMENT,
   `state_name` VARCHAR(45) NOT NULL,
   PRIMARY KEY (`state_id`),
@@ -21,9 +21,9 @@ CREATE TABLE IF NOT EXISTS `Ex2`.`state` (
 ENGINE = InnoDB;
 
 -- -----------------------------------------------------
--- Table `Ex2`.`location`
+-- Table `s11Pizzeria`.`location`
 -- -----------------------------------------------------
-CREATE TABLE IF NOT EXISTS `Ex2`.`location` (
+CREATE TABLE IF NOT EXISTS `s11Pizzeria`.`location` (
   `location_id` INT NOT NULL AUTO_INCREMENT,
   `location_name` VARCHAR(45) NULL,
   `state_state_id` INT NOT NULL,
@@ -32,84 +32,86 @@ CREATE TABLE IF NOT EXISTS `Ex2`.`location` (
   INDEX `fk_location_state1_idx` (`state_state_id` ASC) VISIBLE,
   CONSTRAINT `fk_location_state1`
     FOREIGN KEY (`state_state_id`)
-    REFERENCES `Ex2`.`state` (`state_id`)
+    REFERENCES `s11Pizzeria`.`state` (`state_id`)
     ON DELETE CASCADE
     ON UPDATE CASCADE)
 ENGINE = InnoDB;
 
 -- -----------------------------------------------------
--- Table `Ex2`.`client`
+-- Table `s11Pizzeria`.`client`
 -- -----------------------------------------------------
-CREATE TABLE IF NOT EXISTS `Ex2`.`client` (
+CREATE TABLE IF NOT EXISTS `s11Pizzeria`.`client` (
   `client_id` INT NOT NULL AUTO_INCREMENT,
   `client_name` VARCHAR(45) NOT NULL,
   `client_surname` VARCHAR(45) NOT NULL,
   `client_address` VARCHAR(45) NULL,
   `client_zip` VARCHAR(45) NULL,
-  `client_number` VARCHAR(45) NOT NULL,
+  `client_phone` VARCHAR(45) NOT NULL,
   `location_location_id` INT NOT NULL,
   PRIMARY KEY (`client_id`, `location_location_id`),
-  UNIQUE INDEX `client_number_UNIQUE` (`client_number` ASC) VISIBLE,
+  UNIQUE INDEX `client_phone_UNIQUE` (`client_phone` ASC) VISIBLE,
   UNIQUE INDEX `client_id_UNIQUE` (`client_id` ASC) VISIBLE,
   INDEX `fk_client_locations1_idx` (`location_location_id` ASC) VISIBLE,
-  UNIQUE INDEX `location_location_id_UNIQUE` (`location_location_id` ASC) VISIBLE,
+  INDEX `location_location_id` (`location_location_id` ASC) VISIBLE,
   CONSTRAINT `fk_client_locations1`
     FOREIGN KEY (`location_location_id`)
-    REFERENCES `Ex2`.`location` (`location_id`)
+    REFERENCES `s11Pizzeria`.`location` (`location_id`)
     ON DELETE CASCADE
     ON UPDATE CASCADE)
 ENGINE = InnoDB;
 
 -- -----------------------------------------------------
--- Table `Ex2`.`order`
+-- Table `s11Pizzeria`.`order`
 -- -----------------------------------------------------
-CREATE TABLE IF NOT EXISTS `Ex2`.`order` (
+CREATE TABLE IF NOT EXISTS `s11Pizzeria`.`order` (
   `order_id` INT NOT NULL AUTO_INCREMENT,
   `order_date` DATETIME NOT NULL DEFAULT CURRENT_TIMESTAMP,
   `order_orderType` ENUM('Onsite', 'Delivery') NULL,
   `order_price` DOUBLE NULL,
+  `order_quantity` INT NOT NULL,
   `client_client_id` INT NOT NULL,
   PRIMARY KEY (`order_id`),
   INDEX `fk_order_client1_idx` (`client_client_id` ASC) VISIBLE,
   CONSTRAINT `fk_order_client1`
     FOREIGN KEY (`client_client_id`)
-    REFERENCES `Ex2`.`client` (`client_id`)
+    REFERENCES `s11Pizzeria`.`client` (`client_id`)
     ON DELETE CASCADE
     ON UPDATE CASCADE)
 ENGINE = InnoDB;
 
 -- -----------------------------------------------------
--- Table `Ex2`.`category`
+-- Table `s11Pizzeria`.`category`
 -- -----------------------------------------------------
-CREATE TABLE IF NOT EXISTS `Ex2`.`category` (
+CREATE TABLE IF NOT EXISTS `s11Pizzeria`.`category` (
   `category_id` INT NOT NULL AUTO_INCREMENT,
   `category_name` VARCHAR(45) NOT NULL,
   PRIMARY KEY (`category_id`))
 ENGINE = InnoDB;
 
 -- -----------------------------------------------------
--- Table `Ex2`.`product`
+-- Table `s11Pizzeria`.`product`
 -- -----------------------------------------------------
-CREATE TABLE IF NOT EXISTS `Ex2`.`product` (
+CREATE TABLE IF NOT EXISTS `s11Pizzeria`.`product` (
   `product_id` INT NOT NULL AUTO_INCREMENT,
   `product_name` VARCHAR(45) NOT NULL,
   `product_description` VARCHAR(45) NULL,
   `product_image` VARCHAR(45) NULL,
   `product_price` VARCHAR(45) NOT NULL,
+  `product_type` VARCHAR(45) NOT NULL,
   `category_category_id` INT NOT NULL,
   PRIMARY KEY (`product_id`),
   INDEX `fk_product_category1_idx` (`category_category_id` ASC) VISIBLE,
   CONSTRAINT `fk_product_category1`
     FOREIGN KEY (`category_category_id`)
-    REFERENCES `Ex2`.`category` (`category_id`)
+    REFERENCES `s11Pizzeria`.`category` (`category_id`)
     ON DELETE CASCADE
     ON UPDATE CASCADE)
 ENGINE = InnoDB;
 
 -- -----------------------------------------------------
--- Table `Ex2`.`order_details`
+-- Table `s11Pizzeria`.`order_details`
 -- -----------------------------------------------------
-CREATE TABLE IF NOT EXISTS `Ex2`.`order_details` (
+CREATE TABLE IF NOT EXISTS `s11Pizzeria`.`order_details` (
   `order_details_id` INT NOT NULL AUTO_INCREMENT,
   `order_details_quantity` INT NULL,
   `order_order_id` INT NOT NULL,
@@ -119,20 +121,20 @@ CREATE TABLE IF NOT EXISTS `Ex2`.`order_details` (
   INDEX `fk_order_details_product1_idx` (`product_product_id` ASC) VISIBLE,
   CONSTRAINT `fk_order_details_order1`
     FOREIGN KEY (`order_order_id`)
-    REFERENCES `Ex2`.`order` (`order_id`)
+    REFERENCES `s11Pizzeria`.`order` (`order_id`)
     ON DELETE CASCADE
     ON UPDATE CASCADE,
   CONSTRAINT `fk_order_details_product1`
     FOREIGN KEY (`product_product_id`)
-    REFERENCES `Ex2`.`product` (`product_id`)
+    REFERENCES `s11Pizzeria`.`product` (`product_id`)
     ON DELETE CASCADE
     ON UPDATE CASCADE)
 ENGINE = InnoDB;
 
 -- -----------------------------------------------------
--- Table `Ex2`.`store`
+-- Table `s11Pizzeria`.`store`
 -- -----------------------------------------------------
-CREATE TABLE IF NOT EXISTS `Ex2`.`store` (
+CREATE TABLE IF NOT EXISTS `s11Pizzeria`.`store` (
   `store_id` INT NOT NULL AUTO_INCREMENT,
   `store_address` VARCHAR(45) NULL,
   `store_zip` VARCHAR(45) NULL,
@@ -143,25 +145,25 @@ CREATE TABLE IF NOT EXISTS `Ex2`.`store` (
   INDEX `fk_store_order1_idx` (`order_order_id` ASC) VISIBLE,
   CONSTRAINT `fk_store_location1`
     FOREIGN KEY (`location_location_id`)
-    REFERENCES `Ex2`.`location` (`location_id`)
+    REFERENCES `s11Pizzeria`.`location` (`location_id`)
     ON DELETE CASCADE
     ON UPDATE CASCADE,
   CONSTRAINT `fk_store_order1`
     FOREIGN KEY (`order_order_id`)
-    REFERENCES `Ex2`.`order` (`order_id`)
+    REFERENCES `s11Pizzeria`.`order` (`order_id`)
     ON DELETE CASCADE
     ON UPDATE CASCADE)
 ENGINE = InnoDB;
 
 -- -----------------------------------------------------
--- Table `Ex2`.`worker`
+-- Table `s11Pizzeria`.`worker`
 -- -----------------------------------------------------
-CREATE TABLE IF NOT EXISTS `Ex2`.`worker` (
+CREATE TABLE IF NOT EXISTS `s11Pizzeria`.`worker` (
   `worker_id` INT NOT NULL AUTO_INCREMENT,
   `worker_name` VARCHAR(45) NOT NULL,
   `worker_surname` VARCHAR(45) NOT NULL,
   `worker_nif` VARCHAR(45) NOT NULL,
-  `worker_number` VARCHAR(45) NOT NULL,
+  `worker_phone` VARCHAR(45) NOT NULL,
   `worker_type` ENUM('chef', 'deliver') NOT NULL,
   `store_store_id` INT NOT NULL,
   PRIMARY KEY (`worker_id`),
@@ -170,7 +172,7 @@ CREATE TABLE IF NOT EXISTS `Ex2`.`worker` (
   INDEX `fk_worker_store1_idx` (`store_store_id` ASC) VISIBLE,
   CONSTRAINT `fk_worker_store1`
     FOREIGN KEY (`store_store_id`)
-    REFERENCES `Ex2`.`store` (`store_id`)
+    REFERENCES `s11Pizzeria`.`store` (`store_id`)
     ON DELETE CASCADE
     ON UPDATE CASCADE)
 ENGINE = InnoDB;
